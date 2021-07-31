3aaaef37ec22da7e510c2f99f85b0117304f6474
yingchen <yingchen.lai.0505@gmail.com>
2021-07-31 17:45:46 +0800

    Created parser for day 1 input.

--- src/main/kotlin/aoc2020/Day4.kt
+++ src/main/kotlin/aoc2020/Day4.kt
@@ -1,6 +1,57 @@
 package aoc2020
 
+import java.io.File
+
 class Day4 {
 
+    fun loadInput(path: String): List<Passport> = File(path)
+        .useLines { it.toList().splitByEmptyLines().map { strings -> strings.toPassport() } }
+
+    /**
+     * Convert a list of strings specifying a passport to passport.
+     */
+    fun List<String>.toPassport(): Passport {
+        val string = this.joinToString(" ")
+        return Passport(
+            string.findValueByFieldName("byr"),
+            string.findValueByFieldName("iyr"),
+            string.findValueByFieldName("eyr"),
+            string.findValueByFieldName("hgt"),
+            string.findValueByFieldName("hcl"),
+            string.findValueByFieldName("ecl"),
+            string.findValueByFieldName("pid"),
+            string.findValueByFieldName("cid")
+        )
+    }
+
+    /**
+     * Find value by field name in a string with the format "fieldName:value".
+     */
+    fun String.findValueByFieldName(fieldName: String): String {
+        val regex = """${fieldName}:(\w+)""".toRegex()
+        return regex.find(this)?.groupValues?.getOrElse(1) { "" } ?: ""
+    }
+
+    /**
+     * Split a list of string by empty lines into groups of non-empty strings.
+     */
+    fun List<String>.splitByEmptyLines(): List<List<String>> {
+        val result = mutableListOf<List<String>>()
+        var currentChunk = mutableListOf<String>()
+        for (i in this.indices) {
+            if (this[i].trim().isEmpty()) {
+                result.add(currentChunk)
+                currentChunk = mutableListOf()
+            } else {
+                currentChunk.add(this[i])
+            }
+        }
+        return result
+    }
 
 }
+
+data class Passport(
+    val byr: String, val iyr: String, val eyr: String, val hgt: String, val hcl: String, val ecl: String,
+    val pid: String, val cid: String
+)
--- src/test/kotlin/aoc2020/Day4Test.kt
+++ src/test/kotlin/aoc2020/Day4Test.kt
@@ -1,7 +1,50 @@
 package aoc2020
 
 import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.Test
 
 internal class Day4Test {
 
+    private val day4 = Day4()
+
+    @Test
+    fun loadInput_shouldReturnNonEmptyList() {
+        assertTrue(day4.loadInput("input/day4.txt").isNotEmpty())
+    }
+
+    @Test
+    fun splitByEmptyLines_ShouldReturnCorrectSplitList() {
+        val input = listOf("First line", "Second line", "\n", "Third line", "\n")
+        val expectedResult = listOf(
+            listOf("First line", "Second line"),
+            listOf("Third line")
+        )
+
+        with(Day4()) {
+            assertEquals(expectedResult, input.splitByEmptyLines())
+        }
+    }
+
+    @Test
+    fun toPassports_shouldReturnCorrectPassports() {
+        val input = listOf(
+            "byr:1971",
+            "iyr:2017 hgt:160cm",
+            "eyr:2020 ecl:hzl",
+            "pid:157096267"
+        )
+        val expectedResult = Passport("1971", "2017", "2020", "160cm", "", "hzl", "157096267", "")
+
+        with(Day4()) {
+            assertEquals(expectedResult, input.toPassport())
+        }
+    }
+
+    @Test
+    fun findValueByFieldName_shouldFindByrValue() {
+        val string = "byr:1971 iyr:2017 hgt:160cm"
+        with(Day4()) {
+            assertEquals("1971", string.findValueByFieldName("byr"))
+        }
+    }
 }
